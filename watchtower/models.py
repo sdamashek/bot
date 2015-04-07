b21f1d066a5802aa87c1d13339018ab1ea275022
Fox Wilson <foxwilson123@gmail.com>
2015-04-07 08:35:41 -0400

    start writing models, package watchtower

---
+++ watchtower/models.py
@@ -0,0 +1,10 @@
+from .database import db
+from peewee import *
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Channel(BaseModel):
+    name = TextField()
+    config = TextField() # i am making a terrible mistake (JSON serialized)
