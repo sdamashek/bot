e117f5d17d73ba75e20aa452ed58692af54d921b
The Dank Meme Master <fwilson@fwilson.me>
2015-04-23 22:38:01 -0400

    add stuff

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import run, put, cd, prefix
+
+def deploy():
+    run("killall -USR2 python || echo No python process found")
+    run("rm -rf watchtower-bot || echo No watchtower dir found")
+    run("git clone --depth 1 https://github.com/watchtower/bot watchtower-bot")
+    put(local_path="config.production.ini", remote_path="~/watchtower-bot/config.ini")
+    with cd("~/watchtower-bot/"):
+        run("virtualenv env")
+        with prefix("source env/bin/activate"):
+            run("pip install -r requirements.txt")
+            run("python preinstall.py")
+            run("python run.py")
---
+++ preinstall.py
@@ -0,0 +1,5 @@
+from watchtower.models import Channel, Permission, BlacklistEntry
+
+Channel.create_table()
+Permission.create_table()
+BlacklistEntry.create_table()
