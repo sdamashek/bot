71d186e31320d16b348881892db217d3195959b6
Samuel Damashek <samuel.damashek@gmail.com>
2015-05-08 20:34:04 -0400

    Add support for mysql driver

--- watchtower/database.py
+++ watchtower/database.py
@@ -9,5 +9,9 @@ dbconfig = config["database"]
 
 if dbconfig["driver"] == "sqlite":
     db = SqliteDatabase(dbconfig["database"])
+elif dbconfig["driver"] == "mysql":
+    db = MySQLDatabase(dbconfig["database"], host=dbconfig["host"],
+                       port=dbconfig["port"] if "port" in dbconfig else 3306, user=dbconfig["user"],
+                       password=dbconfig["password"])
 else:
     raise UnknownDriverException("Unknown database driver {}.".format(dbconfig["driver"]))
